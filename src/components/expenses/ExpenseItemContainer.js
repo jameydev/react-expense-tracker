e169ac3f7e5411788734c7094b847bdd4ef0af37
JameyDev <jybryce4@gmail.com>
2023-07-20 20:57:22 -0500

    saving; continuing course soon

--- src/components/expenses/ExpenseItemContainer.js
+++ src/components/expenses/ExpenseItemContainer.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 import './Expenses.css';
 
 export default function ExpenseItemContainer(props) {
+    const  [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
         <Card className="expenses">
+            <ExpenseFilter 
+                onChangeFilter={filterChangeHandler}
+                selected={filteredYear} 
+            />
             {props.expenses.map(expense => 
                 <ExpenseItem 
                     title={expense.title}
