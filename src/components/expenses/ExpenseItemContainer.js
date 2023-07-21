b3e139345baeb1d7b71cdbf03fe7d72dd96e205b
JameyDev <jybryce4@gmail.com>
2023-07-21 11:49:01 -0500

    ExpenseItemContainer logic cleanup

--- src/components/expenses/ExpenseItemContainer.js
+++ src/components/expenses/ExpenseItemContainer.js
@@ -11,20 +11,30 @@ export default function ExpenseItemContainer(props) {
         setFilteredYear(selectedYear);
     }
 
-    return (
-        <Card className="expenses">
-            <ExpenseFilter 
-                onChangeFilter={filterChangeHandler}
-                selected={filteredYear} 
-            />
-            {props.expenses.map(expense => 
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p class="not-found">No expenses found</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = 
+            filteredExpenses.map(expense => 
                 <ExpenseItem 
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
                     key={expense.id} 
                 />
-            )}
+    )}
+
+    return (
+        <Card className="expenses">
+            <ExpenseFilter 
+                onChangeFilter={filterChangeHandler}
+                selected={filteredYear} 
+            />
+            {expensesContent}
         </Card>
     );
     
